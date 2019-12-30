08651c38d67f329b465ed6618e0ec55140ad9f09
Hunter Parks <hunter.parks@pkware.com>
2019-12-30 10:06:40 -0600

    Rework content

--- publi.sh
+++ publi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "Running Publish..."
+echo 'Running Publish...'
 rm -rf ./docs/*.*
 cp -a ./src/. ./docs/
-sed -i "s/%%year%%/$(date '+%Y')/g" ./docs/index.html
-echo "Finished!"
+echo 'Finished!'
